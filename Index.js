cc0cb9fe0debb23eb31e92775b0b0fa520b1f62f
lkllegomangmailcom <lkllegoman@gmail.com>
2022-08-09 17:57:12 -0700

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,29 +1,31 @@
 
 var mode = 'default';
 const request = require('request');
-
+var va= 1;
 let Mic = require('mic');
 console.log('start up');
 const Spe = require('speaker');
+const n = require('naudiodon');
 const mic = Mic({
-        rate:44100,
-        channels:1,
-        device:'hw:3,0',
-        bitDepth:16,
-        debug:false,
-        exitOnSilence:6
-        });
+  endian:BigInt,
+  rate:44100,
+  channels:1,
+  device:'hw:1,0',
+  bitDepth:16,
+  debug:false,
+  exitOnSilence:6
+  });
 
 
 
 
 const spek = new Spe({
-        channels: 1,
-        bitDepth: 16,
-        sampleRate: 44100,
-        device: 'hw:0,0'
+  channels: 1,
+  bitDepth: 16,
+  sampleRate: 44100,
+  device: 'hw:0,0'
 });
-
+console.log(n.getDevices());
 const stream = mic.getAudioStream();
 
 stream.pipe(spek);
@@ -51,6 +53,7 @@ if (mode!="default") {
   
   })
 } else {
+  stream.pipe(spek);
   mic.start();
 }
 }
@@ -69,13 +72,17 @@ function getMode(){
  request.get(authOptions,function(error, response, body) {
     console.log(body)
   if (body.mod!=mode) {
-    
+    after(body.mode);
   }
   
  })
   }
 setInterval(function() {
-
+if (va=1){
+  va=2
+ 
+}
     getMode();
 
 }, 60000);
+
